5470b4b79244d8711e68a669733533abb61c6641
Zuber-Ahmed <zuberahmed333@gmail.com>
2022-05-28 19:00:16 +0530

    going to master

--- src/components/ContainerManager.jsx
+++ src/components/ContainerManager.jsx
@@ -5,9 +5,12 @@ import { Multi } from "./Multi";
 import { Workdone } from "./Workdone";
 import { Even } from "./Even";
 import { Area } from "./Area";
+import { Mileag } from "./Mileage";
+import { TilesBox } from "./TilesBox";
+import { Boggie } from "./Boggie";
 
 export const ContainerManager = () => {
-  const [val, setVal] = useState("add");
+  const [val, setVal] = useState("Boggie");
 
   return (
     <div>
@@ -17,12 +20,18 @@ export const ContainerManager = () => {
         <Tab value="even" label="Even-Odd" />
         <Tab value="WD" label="Work-Done" />
         <Tab value="area" label="AREA" />
+        <Tab value="Travel Cost" label="Travel Cost" />
+        <Tab value="Tilesbox" label="Tiles of Box" />
+        <Tab value="Boggie" label="Boggie Section" />
       </Tabs>
       {val === "add" && <Sum/>}
       {val === "mul" && <Multi/>}
       {val === "even" && <Even/>}
       {val === "WD" && <Workdone/>}
       {val === "area" && <Area/>}
+      {val === "Travel Cost" && <Mileag/>}
+      {val === "Tilesbox" && <TilesBox/>}
+      {val === "Boggie" && <Boggie/>}
     </div>
   );
 };
